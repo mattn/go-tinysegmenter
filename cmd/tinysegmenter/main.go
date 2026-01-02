d6633986936600e79ac9690439f25b5842dd2678
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-02 22:14:43 +0900

    set flags

--- cmd/tinysegmenter/main.go
+++ cmd/tinysegmenter/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -9,9 +10,27 @@ import (
 	"github.com/mattn/go-tinysegmenter"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 func main() {
+	var preserveTokens bool
+	var preserveList arrayFlags
+	flag.BoolVar(&preserveTokens, "p", false, "Preserve tokens in the input")
+	flag.Var(&preserveList, "w", "List of tokens to preserve")
+	flag.Parse()
 	scanner := bufio.NewScanner(os.Stdin)
 	seg := tinysegmenter.New()
+	seg.SetPreserveTokens(preserveTokens)
+	seg.SetPreserveList(preserveList)
 	for scanner.Scan() {
 		fmt.Println(strings.Join(seg.Segment(scanner.Text()), " "))
 	}
